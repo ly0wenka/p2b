f71000eb249b9fab54ab8e4fa9374d04ac16fd18
ly0wenka <ly0wenka@gmail.com>
2021-02-22 17:40:28 +0200

    [Task] #15 [Project] 3D.ML, Agents.ML, Copy.3D.Combat [Chagelog] initial
    [TODO] MoonCat.Scripts, commands.txt

---
+++ Copy.3D.Combat/New Unity Project/Assets/MoonCat.Scripts/SplashScreen.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplashScreen : MonoBehaviour
+{
+    public Texture2D splashScreenBackground;
+    public Texture2D splashScreenText;
+
+    private AudioSource splashScreenAudio;
+    public AudioClip splashScreenMusic;
+
+    private float splashScreenFadeValue;
+    private float splashScreenFadeSpeed = 0.15f;
+
+    private SplashScreenController splashScreenController;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
