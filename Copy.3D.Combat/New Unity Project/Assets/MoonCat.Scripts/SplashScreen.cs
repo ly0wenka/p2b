ca752238cf64dae1faec5857680e5c887435434f
ly0wenka <ly0wenka@gmail.com>
2021-02-25 17:37:30 +0200

    [Task] #16 [Project] Copy.3D.Combat [Changelog] Splash screen part2
    [TODO] MoonCat.Scripts, commands.txt

--- Copy.3D.Combat/New Unity Project/Assets/MoonCat.Scripts/SplashScreen.cs
+++ Copy.3D.Combat/New Unity Project/Assets/MoonCat.Scripts/SplashScreen.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class SplashScreen : MonoBehaviour
 {
     public Texture2D splashScreenBackground;
@@ -15,14 +16,41 @@ public class SplashScreen : MonoBehaviour
 
     private SplashScreenController splashScreenController;
 
+    void Awake()
+    {
+        splashScreenFadeValue = 0;
+    }
+
     void Start()
     {
-        
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+
+        splashScreenAudio = GetComponent<AudioSource>();
+
+        splashScreenAudio.volume = 0;
+        splashScreenAudio.clip = splashScreenMusic;
+        splashScreenAudio.loop = true;
+        splashScreenAudio.Play();
+
+        splashScreenController = SplashScreenController.SplashScreenFadeIn;
+
+        StartCoroutine("SplashScreenManager");
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void SplashScreenFadeIn()
+    {
+        Debug.Log("SplashScreenFadeIn");
+    }
+
+    private void SplasScreenFadeOut()
+    {
+        Debug.Log("SplashScreenFadeOut");
     }
 }
