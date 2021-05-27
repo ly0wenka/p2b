12ed9430db77ebf964c84f17a0785149133c48d4
ly0wenka <ly0wenka@gmail.com>
2021-05-27 17:03:41 +0900

    [Task] #44 [Project] Bombpacity [Changelog] Updates + Tests

---
+++ Bombpacity/Assets/Scripts/View/ControlBarVisibility.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlBarVisibility : MonoBehaviour
+{
+    public Transform controlBar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controlBar.gameObject.SetActive(Application.platform == RuntimePlatform.Android);    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Bombpacity/Assets/Tests/NewTestScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class NewTestScript
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void NewTestScriptSimplePasses()
+        {
+            // Use the Assert class to test conditions
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator NewTestScriptWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+        
+        private Field game;
+
+// 1
+        [UnityTest]
+        public IEnumerator AsteroidsMoveDown()
+        {
+            // 2
+            GameObject gameGameObject = 
+                MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Game"));
+            game = gameGameObject.GetComponent<Field>();
+            // 3
+            // GameObject asteroid = game.GetSpawner().SpawnAsteroid();
+            // // 4
+            // float initialYPos = asteroid.transform.position.y;
+            // 5
+            yield return new WaitForSeconds(0.1f);
+            // 6
+            //Assert.Less(asteroid.transform.position.y, initialYPos);
+            // 7
+            Object.Destroy(game.gameObject);
+        }
+    }
+}
