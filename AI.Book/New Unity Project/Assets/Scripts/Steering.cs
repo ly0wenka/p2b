9457299496ba247e6f787688eeccf3a5825b0e9d
ly0wenka <ly0wenka@gmail.com>
2021-06-07 11:15:32 +0200

    [Task] #56 [Project] AI.Book[Changelog] New project

---
+++ AI.Book/New Unity Project/Assets/Scripts/Steering.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Steering
+{
+    public float angular;
+    public Vector3 linear;
+    public Steering(float angular = 0.0f, Vector3 linear = new Vector3())
+    {
+        this.angular = angular;
+        this.linear = linear;
+    }
+}
