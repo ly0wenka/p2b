0d29adf31830e7cecf1b9ee0a2129b1f7484d3ff
ly0wenka <ly0wenka@gmail.com>
2022-06-14 20:35:18 +0300

    [T] #122 [P] Copy.3D.Combat [C] +ChooseCharacterScreenTest

---
+++ Copy.3D.Combat/MoonCat2020/Assets/Tests/PlayMode/ChooseCharacterScreenTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using Tests.Utils;
+using Tests.Utils.Arranges;
+using UnityEngine.TestTools;
+
+namespace Tests.PlayMode
+{
+    public class ChooseCharacterScreenTest
+    {
+        /// <summary>
+        /// 1. Load Splash Screen
+        /// 2. Press 0 Button at Main Menu Scene
+        /// </summary>
+        /// <returns></returns>
+        [UnityTest]
+        public IEnumerator WhenLoadSceneSplashScreen()
+        {
+            yield return Load.SceneAsync("Splash Screen"); // wait until animations
+            //Assert.AreEqual(false, PlayerOneMovement._playerIsPunchingLeft);
+            yield return Wait.For3Seconds;
+            var mainMenu = Setup.MainMenu();
+            //mainMenu.MainMenuButtonPress();
+            yield return Load.SceneAsync("ChooseCharacter"); // wait until animations
+            yield return Wait.For100MilliSeconds;
+      //      Assert.IsNotNull(button);
+            yield return Wait.For20Seconds;
+        }
+    }
+}
